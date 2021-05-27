c6bb461f335d99bfebcba0ff93f78e7d75f92b2e
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-05-27 14:46:39 +0900

    アプリケーションをモジュールに分割

--- main.js
+++ main.js
@@ -1,33 +1,24 @@
 const program = require("commander");
 const fs = require("fs");
-const marked = require("marked");
+// md2htmlモジュールをインポートする
+const md2html = require("./md2html");
 
-// gfmオプションを定義する
 program.option("--gfm", "GFMを有効にする");
 program.parse(process.argv);
 const filePath = program.args[0];
 
-// コマンドライン引数のオプションを取得する
-const options = program.opts();
-
-// コマンドライン引数で指定されなかったオプションにデフォルト値を上書きする
 const cliOptions = {
-  gfm: options.gfm ?? false,
+  gfm: false,
+  ...program.opts(),
 };
 
 fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
   if (err) {
-    console.error(err.message);
+    console.error(err);
     process.exit(1);
     return;
   }
-  const html = marked(file, {
-    // オプションの値を使用する
-    gfm: cliOptions.gfm,
-  });
+  // md2htmlモジュールを使ってHTMLに変換する
+  const html = md2html(file, cliOptions);
   console.log(html);
 });
-
-// markedパッケージを使ってMarkdown文字列をHTML文字列に変換した
-// コマンドライン引数でmarkedの変換オプションを設定した
-// デフォルトオプションを定義し、コマンドライン引数で上書きできるようにした
