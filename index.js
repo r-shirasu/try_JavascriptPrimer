55a9e7704c3c2aef884e6489afc963e7e802414f
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-05-27 13:20:49 +0900

    promiseを活用する

--- index.js
+++ index.js
@@ -1,33 +1,52 @@
-function fetchUserInfo(userId) {
-  fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
-    .then((response) => {
-      if (!response.ok) {
-        console.error("エラーレスポンス", response);
-      } else {
-        return response.json().then((userInfo) => {
-          // HTMLの組み立て
-          const view = escapeHTML`
-                    <h4>${userInfo.name} (@${userInfo.login})</h4>
-                    <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
-                    <dl>
-                        <dt>Location</dt>
-                        <dd>${userInfo.location}</dd>
-                        <dt>Repositories</dt>
-                        <dd>${userInfo.public_repos}</dd>
-                    </dl>
-                    `;
-          // HTMLの挿入
-          const result = document.getElementById("result");
-          result.innerHTML = view;
-        });
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+async function main() {
+  try {
+    const userId = getUserId();
+    const userInfo = await fetchUserInfo(userId);
+    const view = createView(userInfo);
+    displayView(view);
+  } catch (error) {
+    //   promiseチェーンの中で発生したエラーを受け取る
+    console.error(`エラーが発生しました (${error})`);
+  }
 }
 
-// 特殊な記号に対するエスケープ処理
+function fetchUserInfo(userId) {
+  return fetch(
+    `https://api.github.com/users/${encodeURIComponent(userId)}`
+  ).then((response) => {
+    if (!response.ok) {
+      return Promise.reject(
+        new Error(`${response.status}: ${response.statusText}`)
+      );
+    } else {
+      // JSONオブジェクトで解決されるPromiseを返す
+      return response.json();
+    }
+  });
+}
+
+function getUserId() {
+  return document.getElementById("userId").value;
+}
+
+function createView(userInfo) {
+  return escapeHTML`
+    <h4>${userInfo.name} (@${userInfo.login})</h4>
+    <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+    <dl>
+        <dt>Location</dt>
+        <dd>${userInfo.location}</dd>
+        <dt>Repositories</dt>
+        <dd>${userInfo.public_repos}</dd>
+    </dl>
+    `;
+}
+
+function displayView(view) {
+  const result = document.getElementById("result");
+  result.innerHTML = view;
+}
+
 function escapeSpecialChars(str) {
   return str
     .replace(/&/g, "&amp;")
@@ -47,3 +66,9 @@ function escapeHTML(strings, ...values) {
     }
   });
 }
+
+// HTMLの組み立てと表示の処理をcreateView関数とdisplayView関数に分離した
+// main関数を宣言し、fetchUserInfo関数が返すPromiseのエラーハンドリングを行った
+// Promiseチェーンを使ってfetchUserInfo関数をリファクタリングした
+// Async Function を使ってmain関数をリファクタリングした
+// index.htmlに<input>タグを追加し、getUserId関数でユーザーIDを取得した
