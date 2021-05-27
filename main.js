21baedfe6bf7f490d74a021e547a55a80d3435cb
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-05-27 13:59:16 +0900

    コマンドライン引数を処理する

---
+++ main.js
@@ -0,0 +1,9 @@
+// commanderモジュールをprogramをしてインポートする
+const program = require("commander");
+
+// コマンドライン引数をcommanderでパースする
+program.parse(process.argv);
+
+// ファイルパスをprogram.args配列から取り出す
+const fulePath = program.args[0];
+console.log(filePath);
